b245aa4ec238b226db86948ca051e33d698805ae
shelly <shelykr@gmail.com>
2025-06-18 15:51:11 +0300

    separated files

--- js/alert.js
+++ js/alert.js
@@ -8,14 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
       alert(`${plant.textContent} info.`);
     });
   });
-
-  setInterval(setCounter, 1000
-  );
-
-  function setCounter() {
-    // Your code here
-    // Parameters are purely optional.
-    const counterHtml = document.getElementById("counter");
-    counterHtml.innerHTML = Number(counterHtml.textContent) + 1;
-  };
-  
